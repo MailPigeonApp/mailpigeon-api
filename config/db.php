c75b4fae7fc602aad33b375002d3852ace82706e
Desmond Sofua <mrsofua@gmail.com>
2023-09-24 17:50:36 +0000

    first commit

---
+++ config/db.php
@@ -0,0 +1,16 @@
+<?php
+use Dotenv\Dotenv;
+
+if(file_exists(__DIR__ . '../.env')){   
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '../.env');
+    $dotenv->load();
+}
+
+db()->connect([
+    'dbtype' => $_ENV['DB_TYPE'],
+    'port' => null,
+    'host' => $_ENV['DB_HOST'],
+    'username' => $_ENV['DB_USERNAME'],
+    'password' => $_ENV['DB_PASSWORD'],
+    'dbname' => $_ENV['DB_NAME'],
+]);
---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/config/index.php';
+
+use Leaf\Helpers\Authentication;
+
+app()->cors();
+
+
+app()->get('/', function () {
+	response()->page('./welcome.html');
+});
+
+app()->group('/api', function(){
+	app()->group('/v1', function(){
+		app()->post('/submit', function(){
+			$request = request()->body();
+			$bearer = Leaf\Http\Headers::get("Authorization");
+			$key = substr($bearer, 7);
+			// $auth = Leaf\Http\Headers::get("Authorization");
+
+			// $key = substr($auth, 6);
+
+			// $decoded = base64_decode($key);
+			// list($username,$password) = explode(":",$decoded);
+
+			$keyDetails = db()
+				->select('apikey')
+				->find($key);
+
+			if (!$keyDetails) {
+				response()->exit(
+					[
+						"message" => "Invalid API key"
+					], 401
+				);
+			}
+
+			$fields = db()
+				->select('project', 'fields')
+				->find($keyDetails["projectId"]);
+
+			$decodedFields = json_decode($fields['fields'], true);
+
+			$required = [];
+			$empty = [];
+
+			foreach ($decodedFields as $field) {
+				if ($field['required'] && !isset($request[$field["name"]])) {
+					// $fieldName = $field["name"];
+					$required[] = "Field '" . $field["name"] . "' is required";
+					continue;
+				}
+				if (gettype($request[$field['name']]) != $field['type']) {
+					$empty[] = "Field '" . $field["name"] . "' is not of type " . $field['type'];
+				}
+			}
+
+			if(count($empty) > 0) {
+				response()->exit(
+					$empty, 400
+				);
+			};
+
+			if(count($required) > 0) {
+				response()->exit(
+						$required, 400
+				);
+			};
+
+			db()
+				->insert('submission')
+				->params(
+					[
+						'"projectId"' => $keyDetails["projectId"],
+						'"userId"' => $keyDetails["userId"],
+						"data" => json_encode($request)
+					]
+				)
+				->execute();
+					
+
+			response()->json(
+				[
+					"message" => "Submission successful"
+				], 201, true
+			);
+	});
+});
+});
+
+app()->run();
